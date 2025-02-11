ce07cf4d40afa98bfb3127288d2a99bebd0b1c49
whitestrake <git@whitestrake.net>
2025-02-11 17:06:56 +1000

    Tweak Beszel agent

--- extra/beszel.nix
+++ extra/beszel.nix
@@ -1,39 +1,22 @@
 {
-  pkgs,
   config,
+  pkgs,
   lib,
   ...
-}: let
-  cfg = {
-    user = "beszel";
-    group = "beszel";
-    package = pkgs.unstable.beszel;
-  };
-in {
+}: {
   imports = [../secrets];
   sops.secrets.beszelEnv = {};
-
-  users.groups.${cfg.group} = {};
-  users.users.${cfg.user} = {
-    isSystemUser = true;
-    group = cfg.group;
-    extraGroups =
-      lib.optional config.virtualisation.docker.enable "docker"
-      ++ lib.optional config.virtualisation.podman.enable "podman";
-  };
 
   systemd.services.beszel-agent = {
     description = "Beszel monitoring agent";
     after = ["network-online.target"];
     wants = ["network-online.target"];
     wantedBy = ["multi-user.target"];
-    path = [cfg.package];
     serviceConfig = {
-      User = cfg.user;
-      Group = cfg.group;
+      DymamicUser = true;
       Restart = "always";
       EnvironmentFile = config.sops.secrets.beszelEnv.path;
-      ExecStart = "${cfg.package}/bin/beszel-agent";
+      ExecStart = "${pkgs.unstable.beszel}/bin/beszel-agent";
       StateDirectory = "beszel-agent";
 
       # Security/sandboxing
@@ -50,6 +33,9 @@ in {
       RemoveIPC = "yes";
       RestrictSUIDSGID = "true";
       SystemCallArchitectures = "native";
+      SupplementaryGroups =
+        lib.optional config.virtualisation.docker.enable "docker"
+        ++ lib.optional config.virtualisation.podman.enable "podman";
     };
   };
 }
