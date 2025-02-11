537cb507783b10321d35863bc0ab447e16a5d691
whitestrake <git@whitestrake.net>
2025-02-11 11:01:05 +1000

    Simplify sensu config

--- extra/sensu.nix
+++ extra/sensu.nix
@@ -6,23 +6,17 @@
   imports = [../secrets];
   sops.secrets.sensuEnv = {};
 
-  users.groups.sensu = {};
-  users.users.sensu.isSystemUser = true;
-  users.users.sensu.group = "sensu";
-
   systemd.services.sensu-agent = {
-    wantedBy = ["multi-user.target"];
     after = ["network.target"];
+    wantedBy = ["multi-user.target"];
     description = "Sensu-Go monitoring agent.";
     path = with pkgs; [bash];
     serviceConfig = {
-      User = "sensu";
-      Group = "sensu";
-      Restart = "always";
+      CacheDirectory = "sensu";
+      DynamicUser = "true";
       EnvironmentFile = config.sops.secrets.sensuEnv.path;
-      ExecStart = "${pkgs.sensu-go-agent}/bin/sensu-agent start --backend-url $URL --password $PASS";
+      ExecStart = "${pkgs.sensu-go-agent}/bin/sensu-agent start";
+      Restart = "always";
     };
   };
-
-  systemd.tmpfiles.rules = ["d /var/cache/sensu 0750 sensu sensu"];
 }
