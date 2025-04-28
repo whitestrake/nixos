f07704d1793e118098d8c7ba9abd2220f0e13ffc
whitestrake <git@whitestrake.net>
2025-04-28 11:15:26 +1000

    komodo: override to v1.17.4

--- extra/komodo.nix
+++ extra/komodo.nix
@@ -2,7 +2,21 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  komodo-next = pkgs.unstable.komodo.overrideAttrs (oldAttrs: rec {
+    version = "1.17.4";
+    src = pkgs.fetchFromGitHub {
+      owner = "moghtech";
+      repo = "komodo";
+      tag = "v${version}";
+      hash = "sha256-HV+32Mv9nuAG2jfM3tadMX17wQNt6FeZOsHyiJ7nCDs=";
+    };
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      inherit src;
+      hash = "sha256-IC+mPxoSv6kKKkiJw3UpcTTDlrlhnNpCNyW9XPlrMgA=";
+    };
+  });
+in {
   imports = [../secrets];
   sops.secrets.komodoEnv = {};
 
@@ -14,7 +28,7 @@
     path = with pkgs; [bash docker];
     serviceConfig = {
       EnvironmentFile = config.sops.secrets.komodoEnv.path;
-      ExecStart = "${pkgs.unstable.komodo}/bin/periphery";
+      ExecStart = "${komodo-next}/bin/periphery";
       Restart = "always";
     };
   };
