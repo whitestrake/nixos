db44d653c65de4c67f40ff751d872f59f5c0af84
whitestrake <git@whitestrake.net>
2025-09-10 11:31:09 +1000

    Definitely didn't need to list all those deps

--- extra/check_mk.nix
+++ extra/check_mk.nix
@@ -1,44 +1,7 @@
 {inputs, ...}: {
   nixpkgs.overlays = [
     (final: prev: {
-      checkmk-agent = import "${inputs.check_mk-pr}/pkgs/by-name/ch/checkmk-agent/package.nix" {
-        inherit
-          (final)
-          fetchurl
-          rpmextract
-          stdenv
-          makeWrapper
-          gzip
-          lib
-          nixosTests
-          systemd
-          procps
-          util-linux
-          gnugrep
-          perl
-          coreutils
-          findutils
-          iproute2
-          ethtool
-          multipath-tools
-          gnused
-          python3
-          openssl
-          gawk
-          zfs
-          lvm2
-          openvswitch
-          chrony
-          ipmitool
-          freeipmi
-          dmraid
-          storcli
-          megacli
-          postfix
-          varnish
-          ntp
-          ;
-      };
+      checkmk-agent = final.callPackage "${inputs.check_mk-pr}/pkgs/by-name/ch/checkmk-agent/package.nix" {};
     })
   ];
   imports = ["${inputs.check_mk-pr}/nixos/modules/services/monitoring/cmk-agent.nix"];
