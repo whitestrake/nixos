2ff16b1905e1233ceff3e377c0a8d9544c76bc2d
whitestrake <git@whitestrake.net>
2025-09-10 11:28:00 +1000

    Get building cmk-agent

--- extra/check_mk.nix
+++ extra/check_mk.nix
@@ -1,5 +1,46 @@
 {inputs, ...}: {
-  nixpkgs.overlays = [inputs.check_mk-pr.overlays.default];
-  imports = ["${inputs.check_mk-pr}/nixos/modules/services/monitoring/check_mk_agent.nix"];
-  services.check_mk_agent.enable = true;
+  nixpkgs.overlays = [
+    (final: prev: {
+      checkmk-agent = import "${inputs.check_mk-pr}/pkgs/by-name/ch/checkmk-agent/package.nix" {
+        inherit
+          (final)
+          fetchurl
+          rpmextract
+          stdenv
+          makeWrapper
+          gzip
+          lib
+          nixosTests
+          systemd
+          procps
+          util-linux
+          gnugrep
+          perl
+          coreutils
+          findutils
+          iproute2
+          ethtool
+          multipath-tools
+          gnused
+          python3
+          openssl
+          gawk
+          zfs
+          lvm2
+          openvswitch
+          chrony
+          ipmitool
+          freeipmi
+          dmraid
+          storcli
+          megacli
+          postfix
+          varnish
+          ntp
+          ;
+      };
+    })
+  ];
+  imports = ["${inputs.check_mk-pr}/nixos/modules/services/monitoring/cmk-agent.nix"];
+  services.cmk-agent.enable = true;
 }
