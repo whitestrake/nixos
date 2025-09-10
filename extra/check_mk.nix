a343a15fcaaa7590da544047d9bf039a55815539
whitestrake <git@whitestrake.net>
2025-09-10 11:35:32 +1000

    Get cmk-agent-ctl in path

--- extra/check_mk.nix
+++ extra/check_mk.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   nixpkgs.overlays = [
     (final: prev: {
       checkmk-agent = final.callPackage "${inputs.check_mk-pr}/pkgs/by-name/ch/checkmk-agent/package.nix" {};
@@ -6,4 +10,5 @@
   ];
   imports = ["${inputs.check_mk-pr}/nixos/modules/services/monitoring/cmk-agent.nix"];
   services.cmk-agent.enable = true;
+  environment.systemPackages = [config.services.cmk-agent.package];
 }
