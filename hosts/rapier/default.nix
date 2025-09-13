d7c4f53bbf2743de1dcf17dbe7895e9bcc57b278
whitestrake <git@whitestrake.net>
2025-09-13 15:02:59 +1000

    Initial efforts to get i915-sriov working

--- hosts/rapier/default.nix
+++ hosts/rapier/default.nix
@@ -5,6 +5,7 @@
   ...
 }: {
   imports = [
+    inputs.i915-sriov.nixosModules.default
     inputs.disko.nixosModules.disko
     (import ./disko-configuration.nix {
       disks = ["/dev/vda"];
@@ -29,12 +30,19 @@
     deploy-rs
     nil
     alejandra
+
+    libva-utils
+    intel-gpu-tools
   ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
+
+  # i915 SR-IOV driver
+  boot.extraModulePackages = [pkgs.i915-sriov];
+  boot.kernelParams = ["intel_iommu=on" "i915.enable_guc=3" "module_blacklist=xe"];
 
   # Hostname and TZ
   networking.hostName = "rapier";
