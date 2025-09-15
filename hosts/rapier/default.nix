a5a1e53480134da29495daca9211cde457e73189
whitestrake <git@whitestrake.net>
2025-09-15 15:20:36 +1000

    Add tempus media mount to rapier

--- hosts/rapier/default.nix
+++ hosts/rapier/default.nix
@@ -17,11 +17,8 @@
     ../../extra/docker.nix
     ../../secrets
   ];
+
   system.stateVersion = "25.05";
-
-  # QEMU guest agent
-  services.qemuGuest.enable = true;
-
   environment.systemPackages = with pkgs; [
     sops
     age
@@ -29,6 +26,9 @@
     nil
     alejandra
   ];
+
+  # QEMU guest agent
+  services.qemuGuest.enable = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -40,4 +40,43 @@
   networking.domain = "lab.whitestrake.net";
   networking.hostId = "3ae03bc7";
   time.timeZone = "Australia/Brisbane";
+
+  # mediaserver user
+  users.users.mediaserver.isSystemUser = true;
+  users.users.mediaserver.group = "mediaserver";
+  users.users.mediaserver.uid = 1001;
+  users.groups.mediaserver.gid = 1001;
+
+  sops.secrets."smbCredentials/rapier@tempus" = {};
+  fileSystems = let
+    mkCifs = {
+      device,
+      uid,
+      gid ? uid,
+      credentials ? config.sops.secrets."smbCredentials/rapier@tempus".path,
+    }: {
+      device = device;
+      fsType = "cifs";
+      noCheck = true;
+      options = [
+        "soft"
+        "nofail"
+        "_netdev"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.mount-timeout=5"
+        "x-systemd.device-timeout=5"
+        "file_mode=0660"
+        "dir_mode=0770"
+        "credentials=${credentials}"
+        "uid=${toString uid}"
+        "gid=${toString gid}"
+      ];
+    };
+  in {
+    "/mnt/media" = mkCifs {
+      device = "//tempus.lab.whitestrake.net/Media";
+      uid = config.users.users.mediaserver.uid;
+    };
+  };
 }
