cb8ab4f2a37b965819a263afd365101729fedfd2
whitestrake <git@whitestrake.net>
2025-09-15 10:59:57 +1000

    Set up xinetd workaround for lacking agent controller on aarch64

--- hosts/jaeger/default.nix
+++ hosts/jaeger/default.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
 
@@ -11,6 +15,25 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Set up xinetd for checkmk
+  services.xinetd.enable = true;
+  services.xinetd.services = [
+    {
+      name = "check_mk";
+      port = 6556;
+      protocol = "tcp";
+      user = "root";
+      server = "${config.services.cmk-agent.package}/bin/check_mk_agent";
+      extraConfig = ''
+        type = UNLISTED
+        only_from = .fell-monitor.ts.net
+        disable = no
+      '';
+    }
+  ];
+  # Disable the agent controller that won't work on aarch64
+  systemd.services.cmk-agent-ctl-daemon.enable = lib.mkForce false;
 
   networking.hostName = "jaeger";
   networking.domain = "whitestrake.net";
