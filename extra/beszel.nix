cf9f3b6a89b50202ad11bb9bc36e435086f63a15
whitestrake <git@whitestrake.net>
2025-12-20 21:17:46 +1000

    Push Beszel to v0.17.0

--- extra/beszel.nix
+++ extra/beszel.nix
@@ -9,16 +9,16 @@
   services.beszel.agent = {
     enable = true;
     environmentFile = config.sops.secrets.beszelEnv.path;
-    package = pkgs.unstable.beszel;
-    # package = pkgs.unstable.beszel.overrideAttrs (oldAttrs: rec {
-    #   version = "0.17.0";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "henrygd";
-    #     repo = "beszel";
-    #     tag = "v${version}";
-    #     hash = "sha256-MY/rsWdIiYsqcw6gqDkfA8A/Ied3OSHfJI3KUBxoRKc=";
-    #   };
-    #   vendorHash = "sha256-gfQU3jGwTGmMJIy9KTjk/Ncwpk886vMo4CJvm5Y5xpA=";
-    # });
+    # package = pkgs.unstable.beszel;
+    package = pkgs.beszel.overrideAttrs (oldAttrs: rec {
+      version = "0.17.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "henrygd";
+        repo = "beszel";
+        tag = "v${version}";
+        hash = "sha256-MY/rsWdIiYsqcw6gqDkfA8A/Ied3OSHfJI3KUBxoRKc=";
+      };
+      vendorHash = "sha256-gfQU3jGwTGmMJIy9KTjk/Ncwpk886vMo4CJvm5Y5xpA=";
+    });
   };
 }
