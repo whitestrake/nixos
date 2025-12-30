0656e8ecdb6520c1929872bfd9d6e8e1feca8c3b
whitestrake <git@whitestrake.net>
2025-12-31 03:39:58 +1000

    Revert custom sriov config now upstream loading is fixed

--- extra/i915-sriov.nix
+++ extra/i915-sriov.nix
@@ -1,40 +1,20 @@
 {
   inputs,
-  config,
   pkgs,
   ...
 }: {
-  imports = [inputs.i915-sriov.nixosModules.default];
-
   # i915 SR-IOV driver
+  imports = [inputs.i915-sriov.nixosModules.default];
+  boot.extraModulePackages = [pkgs.i915-sriov];
   boot.kernelParams = ["intel_iommu=on" "i915-sriov.enable_guc=3" "module_blacklist=xe"];
-  boot.initrd.kernelModules = ["i915-sriov"];
-
-  # Override the installPhase step to rename the output package to
-  # a different package so we can early load the kernel module
-  # This has more reliable module loading
-  boot.extraModulePackages = [
-    (pkgs.i915-sriov.overrideAttrs (super: {
-      installPhase = ''
-        ${pkgs.xz}/bin/xz -z -k -f drivers/gpu/drm/i915/i915.ko
-        install -D drivers/gpu/drm/i915/i915.ko.xz $out/lib/modules/${config.boot.kernelPackages.kernel.modDirVersion}/kernel/drivers/gpu/drm/i915-sriov/i915-sriov.ko.xz
-        ${pkgs.xz}/bin/xz -z -k -f compat/intel_sriov_compat.ko
-        install -D compat/intel_sriov_compat.ko.xz $out/lib/modules/${config.boot.kernelPackages.kernel.modDirVersion}/kernel/compat/gpu/drm/i915-sriov/intel_sriov_compat.ko.xz
-      '';
-    }))
-  ];
 
   # Hardware acceleration
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-media-driver # Broadwell+ VA-API driver
       intel-compute-runtime # OpenCL/Tone Mapping
       vpl-gpu-rt # QSV VPL runtime
     ];
-  };
-
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "iHD";
   };
 }
