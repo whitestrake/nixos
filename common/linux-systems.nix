9415adbe1b11327942ff6330dba494956d7d1288
whitestrake <git@whitestrake.net>
2026-01-03 15:21:50 +1000

    refactor: filter out current host from buildMachines

--- common/linux-systems.nix
+++ common/linux-systems.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
@@ -31,34 +32,34 @@
   nix.buildMachines = let
     protocol = "ssh-ng";
     sshUser = "builder";
+    maxJobs = 4;
     sshKey = config.sops.secrets.nixBuilderKey.path;
     supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-  in [
-    {
-      inherit protocol sshUser sshKey supportedFeatures;
-      hostName = "jaeger.fell-monitor.ts.net";
-      system = "aarch64-linux";
-      maxJobs = 4;
-      speedFactor = 1;
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdmMlhib1Q0L0N3L2JWeDdVSkZEZVdsVjNnRVJQZXhKc2hBQ0hSZTlqY3Ygcm9vdEBqYWVnZXI=";
-    }
-    {
-      inherit protocol sshUser sshKey supportedFeatures;
-      hostName = "pascal.fell-monitor.ts.net";
-      system = "x86_64-linux";
-      maxJobs = 8;
-      speedFactor = 2;
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtnQVdRNElXb1NXQml2aUx4RlMrU0lvenVMMXgyRGQwZHZ6TUJKUS9YQkcgcm9vdEBwYXNjYWw=";
-    }
-    {
-      inherit protocol sshUser sshKey supportedFeatures;
-      hostName = "orthus.fell-monitor.ts.net";
-      system = "x86_64-linux";
-      maxJobs = 4;
-      speedFactor = 1;
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUI0YjJjYXpXdWt0OHZyNEV0a1J4b29SQkhoYSswVXVNSTlSejlpeWt3dFcgcm9vdEBvcnRodXM=";
-    }
-  ];
+    systems = [
+      {
+        inherit protocol sshUser sshKey supportedFeatures maxJobs;
+        hostName = "jaeger.fell-monitor.ts.net";
+        system = "aarch64-linux";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdmMlhib1Q0L0N3L2JWeDdVSkZEZVdsVjNnRVJQZXhKc2hBQ0hSZTlqY3Ygcm9vdEBqYWVnZXI=";
+      }
+      {
+        inherit protocol sshUser sshKey supportedFeatures;
+        hostName = "pascal.fell-monitor.ts.net";
+        system = "x86_64-linux";
+        maxJobs = 8;
+        speedFactor = 2;
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtnQVdRNElXb1NXQml2aUx4RlMrU0lvenVMMXgyRGQwZHZ6TUJKUS9YQkcgcm9vdEBwYXNjYWw=";
+      }
+      {
+        inherit protocol sshUser sshKey supportedFeatures maxJobs;
+        hostName = "orthus.fell-monitor.ts.net";
+        system = "x86_64-linux";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUI0YjJjYXpXdWt0OHZyNEV0a1J4b29SQkhoYSswVXVNSTlSejlpeWt3dFcgcm9vdEBvcnRodXM=";
+      }
+    ];
+  in
+    # Don't include the current host in its own buildMachines list
+    lib.filter (x: x.hostName != "${config.networking.hostName}.fell-monitor.ts.net") systems;
   nix.settings.builders-use-substitutes = true;
   nix.settings.connect-timeout = 5;
 
