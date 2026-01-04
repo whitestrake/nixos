88ef082e816f5c33056fc58057efc91bec649528
whitestrake <git@whitestrake.net>
2026-01-04 19:30:52 +1000

    refactor: consolidate xinetd configuration and cmk-agent-ctl-daemon disablement for aarch64

--- extra/check_mk.nix
+++ extra/check_mk.nix
@@ -1,6 +1,8 @@
 {
   inputs,
   config,
+  lib,
+  pkgs,
   ...
 }: {
   nixpkgs.overlays = [
@@ -11,4 +13,24 @@
   imports = ["${inputs.check_mk-pr}/nixos/modules/services/monitoring/cmk-agent.nix"];
   services.cmk-agent.enable = true;
   environment.systemPackages = [config.services.cmk-agent.package];
+
+  # Set up xinetd for checkmk on aarch64
+  # The agent controller doesn't work on aarch64, so we need to use xinetd
+  services.xinetd.enable = lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 true;
+  services.xinetd.services = lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 [
+    {
+      name = "check_mk";
+      port = 6556;
+      protocol = "tcp";
+      user = "root";
+      server = "${config.services.cmk-agent.package}/bin/check_mk_agent";
+      extraConfig = ''
+        type = UNLISTED
+        only_from = .fell-monitor.ts.net
+        disable = no
+      '';
+    }
+  ];
+  # Disable the agent controller that won't work on aarch64
+  systemd.services.cmk-agent-ctl-daemon.enable = lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 (lib.mkForce false);
 }
--- hosts/jaeger/default.nix
+++ hosts/jaeger/default.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{...}: {
   imports = [
     ./hardware-configuration.nix
     ../../users/builder.nix
@@ -19,25 +15,6 @@
 
   # Fixes sshAgentAuth for aarch64 systems
   security.sudo-rs.enable = true;
-
-  # Set up xinetd for checkmk
-  services.xinetd.enable = true;
-  services.xinetd.services = [
-    {
-      name = "check_mk";
-      port = 6556;
-      protocol = "tcp";
-      user = "root";
-      server = "${config.services.cmk-agent.package}/bin/check_mk_agent";
-      extraConfig = ''
-        type = UNLISTED
-        only_from = .fell-monitor.ts.net
-        disable = no
-      '';
-    }
-  ];
-  # Disable the agent controller that won't work on aarch64
-  systemd.services.cmk-agent-ctl-daemon.enable = lib.mkForce false;
 
   networking.hostName = "jaeger";
   networking.domain = "whitestrake.net";
