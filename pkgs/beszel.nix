b428510a957889ef8f5b5da956466f9f3c81c2b5
whitestrake <git@whitestrake.net>
2026-01-12 11:10:33 +1000

    feat: beszel 0.17.0 -> 0.18.1

--- pkgs/beszel.nix
+++ pkgs/beszel.nix
@@ -1,11 +1,11 @@
 {pkgs}:
 pkgs.beszel.overrideAttrs (oldAttrs: rec {
-  version = "0.17.0";
+  version = "0.18.1";
   src = pkgs.fetchFromGitHub {
     owner = "henrygd";
     repo = "beszel";
     tag = "v${version}";
-    hash = "sha256-MY/rsWdIiYsqcw6gqDkfA8A/Ied3OSHfJI3KUBxoRKc=";
+    hash = "sha256-TYTWfXO6Pf5Ok4mSMACbW7xxcsmXWOqo358ykUlCOuo=";
   };
-  vendorHash = "sha256-gfQU3jGwTGmMJIy9KTjk/Ncwpk886vMo4CJvm5Y5xpA=";
+  vendorHash = "sha256-OnCX/0DGtkcACuWxGfIreS6SSx9dKq+feWKSymtkABs=";
 })
