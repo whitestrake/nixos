53c61e40a7780dd6b88f4d50ef92baffdf7edde5
whitestrake <git@whitestrake.net>
2026-01-18 01:48:41 +1000

    fix: revert to base paths (will rely on zfs mounts)

--- extra/attic.nix
+++ extra/attic.nix
@@ -13,20 +13,10 @@
   # Atticd server configuration
   services.atticd = {
     enable = true;
-
-    # Credentials file with ATTIC_SERVER_TOKEN_RS256_SECRET_BASE64
     environmentFile = config.sops.secrets.atticEnv.path;
 
     settings = {
       listen = "[::1]:8080";
-
-      database.url = "sqlite:///storage/atticd/server.db?mode=rwc";
-
-      storage = {
-        type = "local";
-        path = "/storage/atticd/data";
-      };
-
       chunking = {
         nar-size-threshold = 64 * 1024; # 64 KiB
         min-size = 16 * 1024; # 16 KiB
@@ -35,7 +25,4 @@
       };
     };
   };
-
-  # Allow atticd to write to the database directory (not just storage.path)
-  systemd.services.atticd.serviceConfig.ReadWritePaths = ["/storage/atticd"];
 }
