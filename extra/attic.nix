010df33e73ee1b18cbf3e97bc1aa5a56170e4650
whitestrake <git@whitestrake.net>
2026-01-18 03:38:56 +1000

    feat: separate Caddy generic config and only configure the hostname we want here

--- extra/attic.nix
+++ extra/attic.nix
@@ -7,6 +7,7 @@
   imports = [
     inputs.attic.nixosModules.atticd
     ../secrets
+    ./caddy.nix
   ];
 
   environment.systemPackages = [
@@ -31,25 +32,7 @@
   };
 
   # Reverse proxy configuration
-  sops.secrets.caddyEnv = {};
-  services.caddy = {
-    enable = true;
-    environmentFile = config.sops.secrets.caddyEnv.path;
-    package = pkgs.unstable.caddy.withPlugins {
-      plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-      hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
-    };
-    globalConfig = ''
-      acme_dns cloudflare {env.CF_API_TOKEN}
-      email {env.ACME_EMAIL}
-    '';
-    virtualHosts = {
-      "attic.whitestrake.net" = {
-        extraConfig = ''
-          reverse_proxy localhost:8080
-        '';
-      };
-    };
-  };
-  networking.firewall.allowedTCPPorts = [80 443];
+  services.caddy.virtualHosts."attic.whitestrake.net".extraConfig = ''
+    reverse_proxy localhost:8080
+  '';
 }
