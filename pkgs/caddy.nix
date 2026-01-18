52321f54c46508366ec25811c557958875bd4813
whitestrake <git@whitestrake.net>
2026-01-18 18:36:22 +1000

    fix: refactor caddy pkg for nix-update

--- pkgs/caddy.nix
+++ pkgs/caddy.nix
@@ -1,17 +1,33 @@
 {pkgs, ...}: let
   version = "2.10.2";
-in
-  (pkgs.caddy.overrideAttrs (oldAttrs: {
-    inherit version;
-    src = pkgs.fetchFromGitHub {
-      owner = "caddyserver";
-      repo = "caddy";
-      tag = "v${version}";
-      hash = "sha256-KvikafRYPFZ0xCXqDdji1rxlkThEDEOHycK8GP5e8vk=";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "caddyserver";
+    repo = "caddy";
+    tag = "v${version}";
+    hash = "sha256-KvikafRYPFZ0xCXqDdji1rxlkThEDEOHycK8GP5e8vk=";
+  };
+
+  caddyWithPlugins =
+    (pkgs.caddy.overrideAttrs (oldAttrs: {
+      inherit version src;
+      vendorHash = "sha256-wjcmWKVmLBAybILUi8tKEDnFbhtybf042ODH7jEq6r8=";
+    })).withPlugins {
+      plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
+      hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
     };
-    vendorHash = "sha256-wjcmWKVmLBAybILUi8tKEDnFbhtybf042ODH7jEq6r8=";
-  }))
-  .withPlugins {
-    plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-    hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
+in
+  # Wrap in a transparent derivation so 'position' points to this file for nix-update
+  pkgs.stdenv.mkDerivation {
+    pname = "caddy";
+    inherit version src;
+
+    dontUnpack = true;
+
+    passthru = caddyWithPlugins.passthru or {};
+
+    buildCommand = ''
+      cp -a ${caddyWithPlugins} $out
+      chmod -R u+w $out
+    '';
   }
