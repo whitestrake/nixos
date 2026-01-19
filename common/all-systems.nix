45350130c5d5fe9bc461ccd49d825386917d3c66
whitestrake <git@whitestrake.net>
2026-01-20 01:30:26 +1000

    feat: remove attic substituter for now

--- common/all-systems.nix
+++ common/all-systems.nix
@@ -12,12 +12,10 @@
     substituters = [
       "https://cache.garnix.io"
       "https://nix-community.cachix.org"
-      "https://attic.whitestrake.net/main"
     ];
     trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "main:42/nvNP0yFzXsXoIvbI8WmaofHgNYN3LAYNtzfE+Yw8="
     ];
   };
 
