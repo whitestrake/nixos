73489e44558d06fb0ba67f84c7dc78fd024815e6
whitestrake <git@whitestrake.net>
2026-01-29 10:59:29 +1000

    feat: hawser config sops templating and initial rollout

--- extra/hawser.nix
+++ extra/hawser.nix
@@ -5,7 +5,14 @@
   ...
 }: {
   imports = [../secrets];
-  sops.secrets.hawserEnv = {};
+  sops.secrets."hawser/server_url" = {};
+  sops.secrets."hawser/token_${config.networking.hostName}" = {};
+  sops.templates."hawserEnv" = {
+    content = ''
+      DOCKHAND_SERVER_URL=${config.sops.placeholder."hawser/server_url"}
+      TOKEN=${config.sops.placeholder."hawser/token_${config.networking.hostName}"}
+    '';
+  };
 
   systemd.services.hawser = {
     description = "Hawser - Remote Docker Agent for Dockhand";
@@ -17,6 +24,7 @@
 
     environment = {
       DOCKER_SOCKET = lib.mkDefault "/var/run/docker.sock";
+      STACKS_DIR = lib.mkDefault "/opt/docker";
       AGENT_NAME = lib.mkDefault (lib.strings.toSentenceCase config.networking.hostName);
     };
 
@@ -25,13 +33,13 @@
       ExecStart = "${pkgs.myPkgs.hawser}/bin/hawser";
       Restart = "always";
       RestartSec = 10;
-      EnvironmentFile = config.sops.secrets.hawserEnv.path;
+      EnvironmentFile = config.sops.templates.hawserEnv.path;
 
       # Security hardening
       NoNewPrivileges = false;
       ProtectSystem = "strict";
       ProtectHome = true;
-      ReadWritePaths = ["/var/run/docker.sock" "/opt/docker/dockhand"];
+      ReadWritePaths = ["/var/run/docker.sock" "/opt/docker"];
     };
   };
 }
--- hosts/omnius/default.nix
+++ hosts/omnius/default.nix
@@ -14,6 +14,7 @@
     ./hardware-configuration.nix
     ../../extra/docker.nix
     ../../extra/attic.nix
+    ../../extra/hawser.nix
     ../../secrets
   ];
   system.stateVersion = "25.11";
