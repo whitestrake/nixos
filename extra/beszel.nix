28c8907a53b14be37ad3fac63e75866d093031b4
whitestrake <git@whitestrake.net>
2026-02-03 10:29:41 +1000

    feat: update beszel to use dbus rule instead of messagebus group

--- extra/beszel.nix
+++ extra/beszel.nix
@@ -9,22 +9,43 @@
 
   services.beszel.agent = {
     enable = lib.mkDefault true;
-    environmentFile = config.sops.secrets.beszelEnv.path;
     package = pkgs.myPkgs.beszel;
+    environmentFile = config.sops.secrets.beszelEnv.path;
   };
 
+  # Set static user to allow receiving dbus rule
   users.users.beszel-agent.isSystemUser = true;
   users.users.beszel-agent.group = "beszel-agent";
   users.groups.beszel-agent = {};
 
   systemd.services.beszel-agent = {
-    environment.SYSTEM_NAME =
-      lib.mkDefault (lib.strings.toSentenceCase config.networking.hostName);
+    environment = {
+      SYSTEM_NAME = lib.mkDefault (lib.strings.toSentenceCase config.networking.hostName);
+    };
     serviceConfig = {
-      DynamicUser = lib.mkForce false;
-      SupplementaryGroups =
-        ["messagebus"]
-        ++ lib.optionals config.virtualisation.docker.enable ["docker"];
+      SupplementaryGroups = lib.optionals config.virtualisation.docker.enable ["docker"];
     };
   };
+
+  # dbus rule to allow ListUnits
+  services.dbus.packages = [
+    (pkgs.writeTextDir "share/dbus-1/system.d/beszel-agent.conf" ''
+      <?xml version="1.0" encoding="UTF-8"?> <!-- -*- XML -*- -->
+
+      <!DOCTYPE busconfig PUBLIC
+                "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"
+                "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+
+      <busconfig>
+        <policy user="beszel-agent">
+          <allow
+            send_destination="org.freedesktop.systemd1"
+            send_type="method_call"
+            send_path="/org/freedesktop/systemd1"
+            send_member="org.freedesktop.systemd1.Manager.ListUnits"
+          />
+        </policy>
+      </busconfig>
+    '')
+  ];
 }
