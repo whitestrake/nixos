dd0933faee61c2ce12a9d2c5d3fd451a779a1395
whitestrake <git@whitestrake.net>
2026-04-16 10:15:59 +1000

    chore: update komodo to version 2.1.2 and refresh hashes

--- pkgs/komodo.nix
+++ pkgs/komodo.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: let
-  version = "2.1.1";
+  version = "2.1.2";
   src = pkgs.fetchFromGitHub {
     owner = "moghtech";
     repo = "komodo";
     tag = "v${version}";
-    hash = "sha256-kcWFpfvgKOQSQsjKEfiHDj9WjhoBOY6q3eHUpdRhApM=";
+    hash = "sha256-Gq88ludr/l4/UqZ1Qbbdz6U/xvnilU4F4qdLY+u68Ro=";
   };
 in
   pkgs.komodo.overrideAttrs (oldAttrs: {
@@ -12,7 +12,7 @@ in
     cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
       inherit src;
       name = "komodo-${version}";
-      hash = "sha256-RmNAlvKrjqGyzaJRi2hQmVzR+j5HioIYytgS1GBjXGs=";
+      hash = "sha256-H60WYnU9mmNioVZL298UEG7CLPZA4PMMZg3Bj7THaeM=";
     };
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [pkgs.pkg-config];
     buildInputs = (oldAttrs.buildInputs or []) ++ [pkgs.openssl];
