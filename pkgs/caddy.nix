bf4f37478bda5618a6eec98e39c9045202f51f7c
whitestrake <git@whitestrake.net>
2026-05-30 23:31:39 +1000

    fix: upgrade caddy to 2.11.3 and refactor override logic to expose goModules

--- pkgs/caddy.nix
+++ pkgs/caddy.nix
@@ -1,28 +1,34 @@
 {pkgs, ...}: let
-  version = "2.11.2";
+  version = "2.11.3";
 
   src = pkgs.fetchFromGitHub {
     owner = "caddyserver";
     repo = "caddy";
     tag = "v${version}";
-    hash = "sha256-QoGq8+lhaSQuC1VwIYE8h8N/ZC1ozfmIwmsIPk29Jos=";
+    hash = "sha256-7Hgmo7ldDtbwl/acEY/4RNhSGnK/NNcXn+eIm1I8HKg=";
   };
+
+  vendorHash = "sha256-QiZZxYsYFUneZ52TfFKQWJ42lmBofvUTZrHmDBuN2O4=";
+
+  overriddenCaddy = pkgs.caddy.overrideAttrs (oldAttrs: {
+    inherit version src vendorHash;
+  });
 
   caddyWithPlugins =
     (pkgs.caddy.override {
-      caddy = pkgs.caddy.overrideAttrs (oldAttrs: {
-        inherit version src;
-        vendorHash = "sha256-wjcmWKVmLBAybILUi8tKEDnFbhtybf042ODH7jEq6r8=";
-      });
+      caddy = overriddenCaddy;
     }).withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-      hash = "sha256-Gb1nC5fZfj7IodQmKmEPGygIHNYhKWV1L0JJiqnVtbs=";
+      hash = "sha256-BJ4TKGHBddLVHw367Y49IMLCZItb02MlLdfNIMCbjp0=";
     };
 in
   # Wrap in a transparent derivation so 'position' points to this file for nix-update
   pkgs.stdenv.mkDerivation {
     pname = "caddy";
     inherit version src;
+
+    # Expose goModules to nix-update so it can find and update vendorHash
+    goModules = overriddenCaddy.goModules;
 
     dontUnpack = true;
 
