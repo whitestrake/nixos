5239b4fec50f1ae340e7ed062d33adc1acef56a1
whitestrake <git@whitestrake.net>
2026-06-03 12:03:35 +1000

    Consolidate Home Manager config directly into whitestrake user aspect

--- modules/aspects/users/whitestrake.nix
+++ modules/aspects/users/whitestrake.nix
@@ -22,17 +22,136 @@
     };
 
     homeManager = { pkgs, lib, ... }: {
-      imports = [ ./_home.nix ];
-
+      home.stateVersion = "23.11";
       home.sessionVariables = lib.mkMerge [
-        { CLICOLOR = "1"; }
+        {
+          COMPOSE_IGNORE_ORPHANS = "True";
+          EDITOR = lib.mkDefault "hx";
+          BAT_PAGING = "never";
+          BAT_THEME = "TwoDark";
+          CLICOLOR = "1";
+        }
         (lib.mkIf pkgs.stdenv.isDarwin { EDITOR = "antigravity"; })
       ];
 
-      home.shellAliases = lib.mkIf pkgs.stdenv.isDarwin {
-        tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
-        agy = "/Applications/Antigravity.app/Contents/Resources/app/bin/antigravity";
+      home.shellAliases = lib.mkMerge [
+        {
+          l = "eza --long --git-ignore";
+          la = "eza --long --all --all --time-style=long-iso";
+          df = "df -h -xtmpfs -xoverlay";
+        }
+        (lib.mkIf pkgs.stdenv.isDarwin {
+          tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+          agy = "/Applications/Antigravity.app/Contents/Resources/app/bin/antigravity";
+        })
+      ];
+
+      home.packages = with pkgs; [
+        nix-search-cli
+        nixos-rebuild
+        ffmpeg
+        yt-dlp
+        bat
+      ];
+
+      # Autojump
+      programs.zoxide.enable = true;
+      programs.zoxide.enableFishIntegration = true;
+      programs.zoxide.options = ["--cmd j"];
+
+      # Command-line fuzzy finder
+      programs.fzf.enable = true;
+      programs.fzf.enableFishIntegration = true;
+
+      # ls replacement
+      programs.eza.enable = true;
+      programs.eza.enableFishIntegration = true;
+      programs.eza.extraOptions = [
+        "--git"
+        "--group"
+        "--header"
+        "--time-style=relative"
+        "--group-directories-first"
+      ];
+
+      # List files - terminal file manager
+      programs.lf.enable = true;
+
+      programs.tealdeer.enable = true;
+      programs.tealdeer.enableAutoUpdates = true;
+
+      # Fix whatever went wrong with the last command
+      programs.pay-respects.enable = true;
+
+      programs.git.enable = true;
+      programs.git.settings = {
+        user.name = "whitestrake";
+        user.email = "git@whitestrake.net";
+        core.fileMode = false;
+        pull.rebase = true;
+        push.autoSetupRemote = true;
       };
+
+      programs.starship = {
+        enable = true;
+        enableFishIntegration = true;
+        settings.username.format = "[$user]($style) at ";
+        settings.username.show_always = true;
+        settings.container.disabled = true;
+      };
+
+      programs.fish = {
+        enable = true;
+        binds.ctrl-c.command = "cancel-commandline";
+        interactiveShellInit = ''
+          set fish_greeting
+        '';
+        loginShellInit = ''
+          if test -d /opt/docker
+            umask 0002
+            cd /opt/docker
+          end
+        '';
+        functions = {
+          nrr = {
+            description = "Remote nixos-rebuild with SSH agent forwarding";
+            body = ''
+              if test (count $argv) -lt 1
+                echo "Usage: nrr HOST [COMMAND]" >&2
+                return 1
+              end
+              set -l CMD switch
+              if test (count $argv) -gt 1
+                set CMD $argv[2]
+              end
+              NIX_SSHOPTS="-A" nixos-rebuild --flake .#$argv[1] --target-host $argv[1] --build-host $argv[1] --sudo $CMD
+            '';
+          };
+        };
+      };
+
+      programs.helix = {
+        enable = true;
+        settings = {
+          theme = "everblush";
+          editor.true-color = true;
+        };
+        languages = {
+          language-server = {
+            nil.command = "${pkgs.nil}/bin/nil";
+          };
+          language = [
+            {
+              name = "nix";
+              auto-format = true;
+              formatter.command = "${pkgs.alejandra}/bin/alejandra";
+              language-servers = ["nil"];
+            }
+          ];
+        };
+      };
+
+      programs.home-manager.enable = true;
     };
   };
 }
