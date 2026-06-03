75a1b271c8008c68388416fc175427c6fc307ee8
whitestrake <git@whitestrake.net>
2026-06-03 11:50:58 +1000

    Expose custom flake packages output via flake-parts perSystem

---
+++ modules/packages.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }: {
+  perSystem = { pkgs, system, ... }: {
+    packages = import ../pkgs {
+      inherit pkgs;
+      unstablePkgs = import inputs.nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
+  };
+}
