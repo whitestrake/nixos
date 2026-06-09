2f545cfdc25ae878c1b22b0b8ac2f20957c1e43e
whitestrake <git@whitestrake.net>
2026-06-10 01:55:49 +1000

    refactor(hosts): drop redundant kronos tailscale override

--- modules/aspects/hosts/kronos/default.nix
+++ modules/aspects/hosts/kronos/default.nix
@@ -14,9 +14,6 @@
       # Fix for running Windows binaries (Exec format error)
       environment.etc."binfmt.d/WSLInterop.conf".text = ":WSLInterop:M::MZ::/init:PF";
 
-      # Don't need Tailscale as we have it on the Windows host
-      services.tailscale.enable = false;
-
       # Use systemd-resolved for native, robust mDNS resolution
       services.resolved = {
         enable = true;
