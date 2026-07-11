2f47d38549a476d06a3346e44a2b027463a6ab17
whitestrake <git@whitestrake.net>
2026-07-12 00:10:36 +1000

    fix(ci): unblock flake update builds

--- packages/beszel.nix
+++ packages/beszel.nix
@@ -48,6 +48,7 @@ in {
   checkFlags = let
     skippedTests = [
       "TestCollectorStartHelpers/nvtop_collector"
+      "TestCollectorStartHelpers/tegrastats_collector"
       "TestApiRoutesAuthentication/GET_/update_-_shouldn't_exist_without_CHECK_UPDATES_env_var"
       "TestConfigSyncWithTokens"
       "TestServiceUpdateCPUPercent/subsequent_call_calculates_CPU_percentage"
