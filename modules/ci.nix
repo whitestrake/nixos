e0ad7ea25da9a2d45dfc083ecc29565f84842909
whitestrake <git@whitestrake.net>
2026-08-31 19:20:30 +1000

    refactor(ci): expose system-rooted CI aliases

--- modules/ci.nix
+++ modules/ci.nix
@@ -6,59 +6,82 @@
 }: let
   classify = host:
     if host.class == "nixos"
-    then {
-      kind = "nixosConfiguration";
-      platform = "linux";
-    }
+    then "nixosConfigurations"
     else if host.class == "darwin"
-    then {
-      kind = "darwinConfiguration";
-      platform = "darwin";
-    }
+    then "darwinConfigurations"
     else throw "unsupported CI host class: ${host.class}";
 
-  configurations =
+  targets =
     lib.concatLists
     (lib.mapAttrsToList
       (system: hosts:
         lib.mapAttrsToList
-        (name: host: let
-          classification = classify host;
-        in {
+        (name: host: {
           inherit system name;
-          inherit (classification) kind platform;
+          namespace = classify host;
           output =
             (lib.getAttrFromPath host.intoAttr self)
               .config.system.build.toplevel;
         })
         hosts)
-      config.den.hosts);
+      config.den.hosts)
+    ++ [
+      {
+        system = "x86_64-linux";
+        namespace = "checks";
+        name = "flake-file";
+        output = self.checks.x86_64-linux.check-flake-file;
+      }
+      {
+        system = "x86_64-linux";
+        namespace = "checks";
+        name = "treefmt";
+        output = self.checks.x86_64-linux.treefmt;
+      }
+    ];
 
   project = pathFields:
     lib.foldl'
-    (result: configuration: let
-      path = map (field: configuration.${field}) pathFields;
+    (result: target: let
+      path = map (field: target.${field}) pathFields;
     in
       if lib.hasAttrByPath path result
       then throw "duplicate CI projection path: ${lib.concatStringsSep "." path}"
       else
         lib.recursiveUpdate
         result
-        (lib.setAttrByPath path configuration.output))
+        (lib.setAttrByPath path target.output))
     {}
-    configurations;
+    targets;
 
-  bySystem = project ["system" "kind" "name"];
-  configurationsByPlatform = project ["platform" "kind" "name"];
+  systemRoots = project ["system" "namespace" "name"];
 
-  byPlatform = lib.recursiveUpdate configurationsByPlatform {
-    linux.check = {
-      flake-file = self.checks.x86_64-linux.check-flake-file;
-      treefmt = self.checks.x86_64-linux.treefmt;
-    };
-  };
+  mergeCiRoots = roots:
+    lib.foldl'
+    (merged: root:
+      lib.recursiveUpdateUntil
+      (path: lhs: rhs:
+        if
+          lib.isDerivation lhs
+          || lib.isDerivation rhs
+          || !(builtins.isAttrs lhs && builtins.isAttrs rhs)
+        then throw "duplicate CI alias path: ${lib.concatStringsSep "." path}"
+        else false)
+      merged
+      root)
+    {}
+    roots;
 in {
-  flake.ci = {
-    inherit byPlatform bySystem;
-  };
+  flake.ci =
+    systemRoots
+    // {
+      linux = mergeCiRoots [
+        systemRoots.x86_64-linux
+        systemRoots.aarch64-linux
+      ];
+
+      darwin = mergeCiRoots [
+        systemRoots.aarch64-darwin
+      ];
+    };
 }
