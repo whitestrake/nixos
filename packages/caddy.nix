bc1cb7ff6ff561a0cbbc655f1521c177eef28450
whitestrake <9500326+whitestrake@users.noreply.github.com>
2026-09-13 15:20:01 +0000

    chore(pkgs): update packages

--- packages/caddy.nix
+++ packages/caddy.nix
@@ -20,7 +20,7 @@
       caddy = overriddenCaddy;
     }).withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v${cloudflareDnsVersion}"];
-      hash = "sha256-PWadA5qr/gR2qDcT8l8u1Xku7LM2HIfWTLOkzezCYy0=";
+      hash = "sha256-dQvk6ezY6TQ1J7PjhCXnThF/SqVgPwBO8/RXzHCY+js=";
     };
 in
   # Wrap in a transparent derivation so 'position' points to this file for nix-update
