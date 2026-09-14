f22475765612351d0d4a92ff1cb6b91e5f19f7b5
whitestrake <git@whitestrake.net>
2026-09-14 21:31:53 +1000

    feat(monitoring): canary stock cAdvisor on Oculus and Pascal

--- modules/aspects/hosts/oculus/oculus.nix
+++ modules/aspects/hosts/oculus/oculus.nix
@@ -20,6 +20,8 @@
       ];
       system.stateVersion = "25.11";
 
+      services.alloy.telemetry.cadvisorMode = "canary";
+
       # Use grub boot loader
       boot.loader.grub = {
         enable = true;
