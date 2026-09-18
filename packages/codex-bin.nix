9e3037e72ae450220e2b34afbf6a3f0b54256018
whitestrake <9500326+whitestrake@users.noreply.github.com>
2026-09-18 15:29:31 +0000

    chore(pkgs): update packages

--- packages/codex-bin.nix
+++ packages/codex-bin.nix
@@ -3,19 +3,19 @@
   pkgs,
   ...
 }: let
-  version = "0.154.0";
+  version = "0.155.0";
   assets = {
     "aarch64-darwin" = {
       name = "codex-aarch64-apple-darwin";
-      hash = "sha256-NEMQoKWRwbGS4E/v8wQyGmmQfJSYuqrDMcp+FuvO+dc=";
+      hash = "sha256-WlhLfN3CqXCDytpT8Q9bxCMVJrf2QQX2pbuC0BzNukk=";
     };
     "x86_64-linux" = {
       name = "codex-x86_64-unknown-linux-musl";
-      hash = "sha256-1+GLJZeujyQvXzHunpDe70jbye3WNNmGj7ZDXQjAfwI=";
+      hash = "sha256-5BXMOtuUreFujUS03VipIBzDSy7lGl1u3fKjoArstsA=";
     };
     "aarch64-linux" = {
       name = "codex-aarch64-unknown-linux-musl";
-      hash = "sha256-WDtI3zKAQhO9zTOMLlrbBrNDQIIfp1enJswKUk+jPCc=";
+      hash = "sha256-i0qcNWkWxRX3yT+RigG4+hNxvMl1it2/pyO4X77FaUs=";
     };
   };
   system = pkgs.stdenv.hostPlatform.system;
